c50f96b93db2ddfcd291802e1cfd744ba6f6ab89
Martin Freire <martin.vallejo01@gmail.com>
2020-02-29 18:48:24 -0500

    Category list adapter implemented

---
+++ app/src/main/java/com/example/saver/CategoryListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.saver;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.saver.model.Category;
+
+import java.util.LinkedList;
+
+public class CategoryListAdapter extends RecyclerView.Adapter<CategoryListAdapter.CategoryViewHolder> {
+    private final LinkedList<Category> categoryList;
+
+    private LayoutInflater inflater;
+
+    public CategoryListAdapter(Context context, LinkedList<Category> categoryList) {
+        this.inflater = LayoutInflater.from(context);
+        this.categoryList = categoryList;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.categorylist_item, parent, false);
+        return new CategoryViewHolder(itemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        Category current = categoryList.get(position);
+        holder.categoryTotal_textView.setText(current.total().toString());
+        holder.categoryName_textView.setText(current.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    class CategoryViewHolder extends RecyclerView.ViewHolder {
+        public final TextView categoryName_textView;
+        public final TextView categoryTotal_textView;
+
+        final CategoryListAdapter adapter;
+
+        public  CategoryViewHolder(View itemView, CategoryListAdapter adapter) {
+            super(itemView);
+            this.adapter = adapter;
+            categoryName_textView = itemView.findViewById(R.id.categoryName_textView);
+            categoryTotal_textView = itemView.findViewById(R.id.categoryTotal_textView);
+        }
+    }
+}
