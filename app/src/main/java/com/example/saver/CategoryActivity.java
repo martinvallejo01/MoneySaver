d6fab955d10caec8853ccd6aebbb31b285c61957
Martin Freire <martin.vallejo01@gmail.com>
2020-03-01 01:14:45 -0500

    Category Activity: ExpenseListAdapter is now a public inner class

--- app/src/main/java/com/example/saver/CategoryActivity.java
+++ app/src/main/java/com/example/saver/CategoryActivity.java
@@ -5,19 +5,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.saver.model.Category;
+import com.example.saver.model.Expense;
 import com.google.gson.Gson;
 
 import java.util.Calendar;
+import java.util.LinkedList;
 
 public class CategoryActivity extends AppCompatActivity {
     public static final String LOG_TAG = CategoryActivity.class.getSimpleName();
@@ -96,5 +101,47 @@ public class CategoryActivity extends AppCompatActivity {
     private void onDaySet(DatePicker view, int year, int month, int day) {
         String date = year + "/" + (month + 1) + "/" + day;
         datePicker_textView.setText(date);
+    }
+
+    public class ExpenseListAdapter extends RecyclerView.Adapter<ExpenseListAdapter.ExpenseViewHolder> {
+        private final LinkedList<Expense> expenseList;
+        private LayoutInflater inflater;
+
+        public ExpenseListAdapter(Context context, LinkedList<Expense> expenseList) {
+            inflater = LayoutInflater.from(context);
+            this.expenseList = expenseList;
+        }
+
+        @Override
+        public ExpenseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = inflater.inflate(R.layout.expenselist_item, parent, false);
+            return new ExpenseViewHolder(itemView, this);
+        }
+
+        @Override
+        public void onBindViewHolder(ExpenseViewHolder holder, int position) {
+            Expense current = expenseList.get(position);
+            holder.amount_textView.setText(String.format("$%s", current.getAmount().toString()));
+            holder.date_textView.setText(current.getDate().toString());
+            holder.description_textView.setText(current.getDescription());
+        }
+
+        @Override
+        public int getItemCount() {
+            return expenseList.size();
+        }
+
+        class ExpenseViewHolder extends RecyclerView.ViewHolder {
+            TextView description_textView, date_textView, amount_textView;
+            final ExpenseListAdapter adapter;
+
+            public ExpenseViewHolder(View itemView, ExpenseListAdapter adapter) {
+                super(itemView);
+                description_textView = itemView.findViewById(R.id.descriptionList_textView);
+                date_textView = itemView.findViewById(R.id.dateList_textView);
+                amount_textView = itemView.findViewById(R.id.amountList_textView);
+                this.adapter = adapter;
+            }
+        }
     }
 }
